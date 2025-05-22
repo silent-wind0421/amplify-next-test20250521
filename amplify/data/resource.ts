e8813f3b0402834709fff69f7a95c0cd3e91cbe7
吉田 明弘 <yoshida@makotoya.tech>
2025-05-22 15:54:50 +0900

    resource_mod

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,11 +1,5 @@
 import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
 
-/*== STEP 1 ===============================================================
-The section below creates a Todo database table with a "content" field. Try
-adding a new "isDone" field as a boolean. The authorization rule below
-specifies that any user authenticated via an API key can "create", "read",
-"update", and "delete" any "Todo" records.
-=========================================================================*/
 const schema = a.schema({
   Todo: a
     .model({
@@ -13,14 +7,13 @@ const schema = a.schema({
     })
     .authorization((allow) => [allow.publicApiKey()]),
 
-  
+  Auth: a
+    .model({
+      username: a.string().required(),
+      loginTime: a.datetime().required(),
+    })
+    .authorization((allow) => [allow.authenticated()]),
 
-  Auth: a.model({
-    username: a.string().required(),
-    loginTime: a.datetime().required(),
-  }).authorization((allow) => [allow.authenticated()]), // 認証ユーザーだけ保存可能
-  
-  
   Post: a.customType({
     id: a.id().required(),
     author: a.string().required(),
@@ -31,8 +24,7 @@ const schema = a.schema({
     downs: a.integer(),
     version: a.integer(),
   }),
-  
-  
+
   addPost: a
     .mutation()
     .arguments({
@@ -43,7 +35,7 @@ const schema = a.schema({
       url: a.string(),
     })
     .returns(a.ref("Post"))
-    .authorization(allow => [allow.publicApiKey()])
+    .authorization((allow) => [allow.publicApiKey()])
     .handler(
       a.handler.custom({
         dataSource: "ExternalPostTableDataSource",
@@ -51,12 +43,11 @@ const schema = a.schema({
       })
     ),
 
-      
   getPost: a
     .query()
     .arguments({ id: a.id().required() })
     .returns(a.ref("Post"))
-    .authorization(allow => [allow.publicApiKey()])
+    .authorization((allow) => [allow.publicApiKey()])
     .handler(
       a.handler.custom({
         dataSource: "ExternalPostTableDataSource",
@@ -64,8 +55,6 @@ const schema = a.schema({
       })
     ),
 
-
-  
   updatePost: a
     .mutation()
     .arguments({
@@ -77,7 +66,7 @@ const schema = a.schema({
       expectedVersion: a.integer().required(),
     })
     .returns(a.ref("Post"))
-    .authorization(allow => [allow.publicApiKey()])
+    .authorization((allow) => [allow.publicApiKey()])
     .handler(
       a.handler.custom({
         dataSource: "ExternalPostTableDataSource",
@@ -85,27 +74,21 @@ const schema = a.schema({
       })
     ),
 
-     
   deletePost: a
     .mutation()
-    .arguments({ id: a.id().required(), expectedVersion: a.integer() })
+    .arguments({
+      id: a.id().required(),
+      expectedVersion: a.integer(),
+    })
     .returns(a.ref("Post"))
-    .authorization(allow => [allow.publicApiKey()])
+    .authorization((allow) => [allow.publicApiKey()])
     .handler(
       a.handler.custom({
         dataSource: "ExternalPostTableDataSource",
         entry: "./deletePost.js",
       })
-    ), 
-
-
-
-
-}
-
-
-
-);
+    ),
+});
 
 export type Schema = ClientSchema<typeof schema>;
 
@@ -118,32 +101,3 @@ export const data = defineData({
     },
   },
 });
-
-/*== STEP 2 ===============================================================
-Go to your frontend source code. From your client-side code, generate a
-Data client to make CRUDL requests to your table. (THIS SNIPPET WILL ONLY
-WORK IN THE FRONTEND CODE FILE.)
-
-Using JavaScript or Next.js React Server Components, Middleware, Server 
-Actions or Pages Router? Review how to generate Data clients for those use
-cases: https://docs.amplify.aws/gen2/build-a-backend/data/connect-to-API/
-=========================================================================*/
-
-/*
-"use client"
-import { generateClient } from "aws-amplify/data";
-import type { Schema } from "@/amplify/data/resource";
-
-const client = generateClient<Schema>() // use this Data client for CRUDL requests
-*/
-
-/*== STEP 3 ===============================================================
-Fetch records from the database and use them in your frontend component.
-(THIS SNIPPET WILL ONLY WORK IN THE FRONTEND CODE FILE.)
-=========================================================================*/
-
-/* For example, in a React component, you can use this snippet in your
-  function's RETURN statement */
-// const { data: todos } = await client.models.Todo.list()
-
-// return <ul>{todos.map(todo => <li key={todo.id}>{todo.content}</li>)}</ul>
