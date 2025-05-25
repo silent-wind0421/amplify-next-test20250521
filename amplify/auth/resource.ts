49a0b6fea44534e82b6a455760522bddd1e63e7a
吉田 明弘 <yoshida@makotoya.tech>
2025-05-26 00:12:31 +0900

    modify20250525b

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,11 +1,15 @@
-import { defineAuth } from '@aws-amplify/backend';
+import { referenceAuth } from '@aws-amplify/backend';
 
-/**
- * Define and configure your auth resource
- * @see https://docs.amplify.aws/gen2/build-a-backend/auth
- */
-export const auth = defineAuth({
-  loginWith: {
-    email: true,
-  },
+export const auth = referenceAuth({
+
+  userPoolId: "ap-northeast-1_z60CJDdU7",
+  userPoolClientId: "6gnv9qldhuos82bvc7gkcudp7m",
+  identityPoolId: "ap-northeast-1:8390aebf-9353-4adf-9ada-0b096192993f",
+  authRoleArn: 'arn:aws:iam::845531086046:role/service-role/IAMFullAccess',
+  /*
+  userPoolId: 'us-east-1_xxxx',
+  identityPoolId: 'us-east-1:b57b7c3b-9c95-43e4-9266-xxxx',
+  authRoleArn: 'arn:aws:iam::xxxx:role/amplify-xxxx-mai-amplifyAuthauthenticatedU-xxxx',
+  unauthRoleArn: 'arn:aws:iam::xxxx:role/amplify-xxxx-mai-amplifyAuthunauthenticate-xxxx',
+  userPoolClientId: 'xxxx',*/
 });
