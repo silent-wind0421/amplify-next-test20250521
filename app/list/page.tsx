a7760172ad51ec6a7ce6de5d6dde4fbcf08cee8c
吉田 明弘 <yoshida@makotoya.tech>
2025-06-24 16:47:05 +0900

    modified20250624

--- app/list/page.tsx
+++ app/list/page.tsx
@@ -1,7 +1,34 @@
 //import "./app-list.css" ; // added 20250606
+'use client'
 
 import AttendanceManagement from "@/components/attendance-management"
+import { useEffect, useState } from 'react';
+import { useAuthenticator } from '@aws-amplify/ui-react';
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
+
+  const { authStatus } = useAuthenticator((context) => [context.authStatus]);
+  const router = useRouter();
+  const [isReady, setIsReady] = useState(false); // 描画制御用
+  
+  // 認証済みなら画像URLを設定
+  useEffect(() => {
+    if (authStatus === 'authenticated') {
+      setIsReady(true); // 認証OK → 描画許可
+      console.log("✅ 認証されています");
+    }
+  }, [authStatus, router]);
+
+  // 未認証ならリダイレクト
+  useEffect(() => {
+    if (authStatus !== 'authenticated' && authStatus !== 'configuring') {
+      console.log("🔒 未認証のためトップにリダイレクトします");
+      router.replace('/');
+    }
+  }, [authStatus, router]);
+
+  if (!isReady) return null;
+
   return <AttendanceManagement />
 }
