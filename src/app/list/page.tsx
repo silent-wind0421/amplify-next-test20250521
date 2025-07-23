384af7b55920cafc2cc05fbf6e19265b6bdd85b2
吉田 明弘 <yoshida@makotoya.tech>
2025-07-23 16:22:02 +0900

    modified20250723a

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -5,11 +5,41 @@
 import AttendanceManagement from "@/components/attendance-management";
 import { SeedButton } from "@/components/seed-button";
 
+// modified by yoshida
+import { useEffect, useState } from 'react';
+import { useAuthenticator } from '@aws-amplify/ui-react';
+import { useRouter } from 'next/navigation';
+
 export default function AttendancePage() {
-  return (
+
+         const { authStatus } = useAuthenticator((context) => [context.authStatus]);
+         const router = useRouter();
+         const [isReady, setIsReady] = useState(false); // 描画制御用
+  
+        // 認証済みなら画像URLを設定
+         useEffect(() => {
+            if (authStatus === 'authenticated') {
+                setIsReady(true); // 認証OK → 描画許可
+                console.log("✅ 認証されています");
+            }
+          }, [authStatus, router]);
+
+        // 未認証ならリダイレクト
+          useEffect(() => {
+            if (authStatus !== 'authenticated' && authStatus !== 'configuring') {
+                console.log("🔒 未認証のためトップにリダイレクトします");
+                router.replace('/');
+            }
+          }, [authStatus, router]);
+
+          if (!isReady) return null;
+
+  
+  
+          return (
    
-      
-        <div className="space-y-4">
+
+          <div className="space-y-4">
           
 
           {process.env.NEXT_PUBLIC_SHOW_SEED_BUTTON === "true" && (
@@ -19,7 +49,7 @@ export default function AttendancePage() {
           )}
 
           <AttendanceManagement />
-        </div>
+          </div>
     
   );
 }
