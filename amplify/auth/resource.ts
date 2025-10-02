ab7de2d897d88bb7d6a2ee571e5de48f601dfd12
吉田 明弘 <yoshida@makotoya.tech>
2025-10-03 02:05:50 +0900

    modified1002aa

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -3,7 +3,7 @@ import { referenceAuth } from '@aws-amplify/backend';
 export const auth = referenceAuth({
   userPoolId: 'ap-northeast-1_IVHfj8jlH',
   identityPoolId: 'ap-northeast-1:d7fa0f56-b34b-40d6-9182-373b0c72918c',
-  authRoleArn: 'arn:aws:iam::845531086046:role/service-role/useraccess',
+  authRoleArn: 'arn:aws:iam::845531086046:role/service-role/adminrole',
   unauthRoleArn: 'arn:aws:iam::845531086046:role/service-role/guest',
   userPoolClientId: '31l0k63r1ifgc20j56m0i3korm',
     
